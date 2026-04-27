6d18d9469047b3fe8963cc0249b6b9e066d98f72
swagat-sinha <swagatsinha03@gmail.com>
2026-04-27 22:49:12 +0530

    Add: combinations - submission-0

---
+++ Data Structures & Algorithms/combinations/submission-0.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<Integer> currComb = new ArrayList<>();
+        List<List<Integer>> combs = new ArrayList<>();
+        combineHelper(1,n,k,currComb,combs);
+        return combs;
+    }
+
+
+    private void combineHelper(int i,int n , int k , List<Integer> currComb, List<List<Integer>>combs){
+
+        if(currComb.size()==k){
+            combs.add(new ArrayList<>(currComb));
+            return;
+        }
+
+        if(i>n){
+            return;
+        }
+
+        currComb.add(i);
+        combineHelper(i+1,n,k,currComb,combs);
+        currComb.remove(currComb.size()-1);
+        combineHelper(i+1,n,k,currComb,combs);
+    }
+}
