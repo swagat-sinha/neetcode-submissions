38ab576450837365f3ed4c136943412c8a809df3
swagat-sinha <swagatsinha03@gmail.com>
2026-08-02 23:57:19 +0530

    Add: validate-parentheses - submission-3

---
+++ Data Structures & Algorithms/validate-parentheses/submission-3.java
@@ -0,0 +1,34 @@
+class Solution {
+    Map<Character, Character> mp = new HashMap<>();
+
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        mp.put('(', ')');
+        mp.put('{', '}');
+        mp.put('[', ']');
+
+        for (char ch : s.toCharArray()) {
+            if (isOpeningBracket(ch)) {
+                st.push(ch);
+            } else {
+                if(st.isEmpty()){
+                    return false;
+                }
+                if ( !isMatchingBracket(st.peek(), ch)) {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.isEmpty();
+    }
+
+    private boolean isOpeningBracket(char ch) {
+    
+        return ch == '(' || ch == '{' || ch == '[' ;
+    }
+
+    private boolean isMatchingBracket(char a, char b) {
+        return mp.get(a).equals(b);
+    }
+}
